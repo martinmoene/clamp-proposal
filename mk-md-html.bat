3748277c11f4d3f027d98feb1462c14055f9bd9b
Martin Moene <martin.moene@gmail.com>
2014-07-06 11:13:25 +0200

    Add title (abuse Pandoc's --title-prefix)

--- mk-md-html.bat
+++ mk-md-html.bat
@@ -2,10 +2,12 @@
 
 setlocal
 
+set title=clamp: An algorithm to 'clamp' a value between a pair of boundary values (Draft)
+
 for %%f in (*.md) do (
 
    echo %%f:
-   pandoc -f markdown -t html5 --standalone --self-contained --base-header-level=1 --include-in-header=mk-md-html.css -o %%~nf.html %%f
+   pandoc -f markdown -t html5 --standalone --self-contained --title-prefix="%title%" --base-header-level=1 --include-in-header=mk-md-html.css -o %%~nf.html %%f
 )
 
 endlocal
